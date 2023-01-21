5094edae6e4a79b56e809308270a0ff9d0027943
eduardo <cezamperlini@gmail.com>
2023-01-21 14:22:16 -0300

    ajustes layout forms

--- lib/home.dart
+++ lib/home.dart
@@ -33,7 +33,7 @@ class _HomePageState extends State<HomePage> {
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const Register()),
+                      MaterialPageRoute(builder: (context) => Register()),
                     );
                   },
                   style: ButtonStyle(
--- lib/register.dart
+++ lib/register.dart
@@ -8,12 +8,98 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  get controle => TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Cadastrar novo",
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cadastro de Cliente"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: const [
+              CustomForm(
+                label: "Nome",
+                iconeform: Icons.person,
+                textoindicativo: "Digite o nome",
+              ),
+              SizedBox(height: 20),
+              CustomForm(
+                label: "Celular",
+                iconeform: Icons.phone_android,
+                textoindicativo: "Digite o celular (48999999999)",
+              ),
+              SizedBox(height: 20),
+              CustomForm(
+                label: "Endereço",
+                iconeform: Icons.home,
+                textoindicativo: "Digite o endereço completo",
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CustomForm(
+                label: "Data de Nascimento",
+                iconeform: Icons.date_range,
+                textoindicativo: "Digite a data de nascimento",
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                width: double.infinity,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: null,
+                  child: Text(
+                    "Salvar",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: null,
+                  child: Text(
+                    "Limpar",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ]),
+      ),
+    );
+  }
+}
+//
+//
+//
+// ? componetizando o TextFormField.
+
+class CustomForm extends StatelessWidget {
+  final String label;
+  final IconData? iconeform;
+  final String? textoindicativo;
+  const CustomForm(
+      {super.key, required this.label, this.iconeform, this.textoindicativo});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autofocus: true,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: iconeform == null ? null : Icon(iconeform),
+        hintText: textoindicativo,
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
         ),
       ),
     );
